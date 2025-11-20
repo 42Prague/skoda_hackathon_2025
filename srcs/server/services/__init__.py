11884fedf8485559681ab8eb1eb3f3da1daeb92f
Nikita <luchininnn@gmail.com>
2025-11-20 18:16:42 +0100

    update services

--- srcs/server/services/__init__.py
+++ srcs/server/services/__init__.py
@@ -4,5 +4,6 @@ Services package initialization
 
 from .employee_service import EmployeeService
 from .skill_service import SkillService
+from .position_service import PositionService
 
-__all__ = ['EmployeeService', 'SkillService', 'DiagramCalculator']
+__all__ = ['EmployeeService', 'SkillService', 'PositionService']
